1ae7537adaef05809d08a9ec0e9e764ef60594a8
ChrisFong604 <ccffoonngg@gmail.com>
2021-07-20 15:15:40 -0700

    updated variable names in Project.java

--- src/main/java/com/example/Project.java
+++ src/main/java/com/example/Project.java
@@ -5,23 +5,24 @@ import java.util.ArrayList;
 
 public class Project {
     private String name;
-    private Date start;
-    private Date end;
+    private Date projectStart;
+    private Date projectEnd;
     private Double workload;
     private ArrayList<Date> Weeks;
 
-    public Project() {}
+    public Project() {
+    }
 
     public void setName(String name) {
         this.name = name;
     }
 
     public void setStart(Date start) {
-        this.start = start;
+        this.projectStart = start;
     }
 
     public void setEnd(Date end) {
-        this.end = end;
+        this.projectEnd = end;
     }
 
     public void setWorkLoad(Double workload) {
@@ -32,12 +33,12 @@ public class Project {
         return this.name;
     }
 
-    public Date getStart() {
-        return this.start;
+    public Date getProjectStart() {
+        return this.projectStart;
     }
 
     public Date getEnd() {
-        return this.end;
+        return this.projectEnd;
     }
 
     public Double getWorkLoad() {
